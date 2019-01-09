65e3c7edde00b459076d8370a2256a1e9388de6b
hermeslin <mercury.iwork@gmail.com>
2019-01-09 18:06:34 +0800

    feature: upgrade bitmark-trade
    upgrade bitmark-trade (0.13-1)

--- bitmark-trade/entrypoint.sh
+++ bitmark-trade/entrypoint.sh
@@ -3,6 +3,7 @@ cat >> /root/bitmark-trade-config <<EOL
 chain = "$BITMARK_TRADE_CHAIN"
 port = 8080
 datadir = "/root/bitmark"
+api_token = "$BITMARK_TRADE_API_TOKEN"
 EOL
 
 ## start bitmark-trade service
